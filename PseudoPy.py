ef4359ada7cd9f204d1d3b137996d5b6e8bc2d8d
ninjagolloyd <ninjagolloyd14@gmail.com>
2015-11-27 17:02:38 -0500

    Add turtle

--- PseudoPy.py
+++ PseudoPy.py
@@ -1,4 +1,11 @@
-m = input("What would you like to do: print")
+import turtle
+m = input("What would you like to do: print draw")
 if m == print:
     a = input("What would you like to print?")
     print(a)
+elif m == "draw":
+    a = input("What would you like to draw: circle")
+    if a == "circle":
+        myTurtle = turtle.Turtle()
+        myTurtle.circle(50)
+        turtle.getscreen()._root.mainloop()
